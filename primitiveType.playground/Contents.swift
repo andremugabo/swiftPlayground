a63c1b442da263333d64d5c5ba9610232aa020a1
Mugabo Andre <andremugabo@yahoo.fr>
2025-02-16 12:33:15 +0200

    String

--- primitiveType.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-var greeting = "Hello, playground"
-print(greeting)
-let int1 : Int = Int(9.5)
-let int2 : Int = 500
-let int3 : UInt = 1000
-
-
---
+++ primitiveType.playground/Pages/Untitled Page.xcplaygroundpage/Primitive.swift
@@ -0,0 +1,25 @@
+
+
+var greeting = "Hello, playground"
+print(greeting)
+let int1 : Int = Int(9.5)
+let int2 : Int = 500
+let int3 : UInt = 1000
+let float1: Float = 54.5
+let double1: Double = 54.6
+let defaultDouble : Double = 0
+//let cgfloat : CGFloat = 202
+print(defaultDouble)
+
+
+// Boolean type  represent true or false values
+
+let bool1 : Bool = false
+let bool2 : Bool = true
+let bool3 : Bool = 5 > 3
+
+// character one digit in lenght, betwwen ""
+
+let char1 : Character = "?"
+let char2 : Character = "😂"
+
